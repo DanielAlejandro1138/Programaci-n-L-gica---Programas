d2e3cc1066923e838e8d50ed0a09380bfe4132a9
DanielAlejandro1138 <47582933+DanielAlejandro1138@users.noreply.github.com>
2019-02-13 22:52:44 -0600

    Add files via upload

---
+++ Programa 1_PYTHON/Programa_1.py
@@ -0,0 +1,8 @@
+edad=input("Cual es tu edad?  ")
+edad=int(edad)
+if edad<18:
+    print("Eres menor de edad")
+    print("No puedes pasar")
+else:
+    print("Eres mayor de edad")
+    print("Puedes pasar")
---
+++ Programa 4_PROLOG/Programa_4.pl
@@ -0,0 +1,7 @@
+legusta(daniel,cristy).
+legusta(cristy,cantar).
+legusta(sakura,bailar).
+legusta(daniel,musica).
+legusta(cristy,musica).
+legusta(sakura,daniel).
+
